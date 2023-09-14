90d1cb690f1838a24d097fe50a2f47bb92d6b154
sanieni6 <luissanchezzamora23@gmail.com>
2023-09-13 22:53:47 -0500

    Add router and all the routes

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Services from "../pages/Services";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Doctors from "../pages/Doctors/Doctors.jsx";
+import DoctorDetails from "../pages/Doctors/DoctorDetails.jsx";
+import Contact from "../pages/Contact";
 
 const Routers = () => {
-  return <div>Routers</div>;
+  return (
+    <Routes>
+        <Route path="/" element= {<Home />} />
+        <Route path="/home" element= {<Home />} />
+        <Route path="/doctors" element= {<Doctors />} />
+        <Route path="/doctors/:id" element= {<DoctorDetails />} />
+        <Route path="/login" element= {<Login />} />
+        <Route path="/register" element= {<Signup />} />
+        <Route path="/contact" element= {<Contact />} />
+        <Route path="/services" element= {<Services />} />
+    </Routes>
+
+  );
 };
 
 export default Routers;
