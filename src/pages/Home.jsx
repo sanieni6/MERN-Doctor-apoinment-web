ede76f1f09dcc64a12c90ff06b2072f89cc332a0
sanieni6 <luissanchezzamora23@gmail.com>
2023-09-15 22:57:13 -0500

    First section home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+    <>
+    <section className="hero__section pt-[60px] 2xl:h-[800px]">
+      <div className="container">
+        <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+          <div>
+            <div className="lg:w-[570px]"> 
+              <h1 className="text-[36px] leading-[46px] text-headingColor font-[800px] 
+              md:text-[60px] md:leading-[70px]">
+                We help patients live a healthy, longer life.
+              </h1>
+              <p className="text__para">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Veritatis ex enim expedita a natus praesentium pariatur libero cum dolor nesciunt. 
+                Ipsa quos cum vitae quae minus, ipsam eligendi maiores necessitatibus.
+              </p>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+    
+    </>
+
+    </>
+  )
 };
 
 export default Home;
