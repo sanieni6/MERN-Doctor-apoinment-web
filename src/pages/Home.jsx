25d266c20fbbacfe1cd56a67f2108c49c2a8f687
sanieni6 <luissanchezzamora23@gmail.com>
2023-09-18 12:08:15 -0500

    implement second session at home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,55 @@ import React from "react";
 import heroImg01 from "../assets/images/hero-img01.png";
 import heroImg02 from "../assets/images/hero-img02.png";
 import heroImg03 from "../assets/images/hero-img03.png";
+import icon01 from '../assets/images/icon01.png';
+import icon02 from '../assets/images/icon02.png';
+import icon03 from '../assets/images/icon03.png';
+import { Link } from "react-router-dom";
+import { BsArrowRight } from "react-icons/bs";
 
 const Home = () => {
+
+  const paragraph = "World-class care for everyone. Our health System offers unmatched, expert health care.From the lab to the clinic.";
+  const servicesLinks = [
+    ['Find a Doctor', paragraph, '/doctors', icon01],
+    ['Find a Location', paragraph, '/locations', icon02],
+    ['Find a Treatment', paragraph, '/treatments', icon03],
+  ];
+
+  const Services = () => {
+    return (
+      <>
+        {servicesLinks.map((service, i) => (
+      
+        <div key={i} className="py-[30px] px-5">
+            <div className="flex items-center justify-center">
+              <img src={service[3]} alt="" />
+            </div>
+
+            <div className="mt-[30px]">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+              {service[0]}
+              </h2>
+              <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+              {service[1]}
+              </p>
+
+              <Link to={service[2]}
+              className="w-[44px] h-[44px] rounded-full border border-solid border-[#181a1e] mt-[30px]
+              mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+              >
+                < BsArrowRight className="group-hover:text-white w-6 h-5"/>
+              </Link>
+
+            </div>
+
+          </div>
+          ))}
+      </>
+        )
+      
+    };
+
   return (
     <>
     <>
@@ -73,7 +120,24 @@ const Home = () => {
       </div>
 
     </section>
-    
+    <section>
+      <div className="container">
+        <div className="lg:w-[470px] mx-auto">
+        <h2 className="heading text-center">
+          Providing the best medical services
+        </h2>
+        <p className="text__para text-center">
+          World-class care for everyone. Our health System offers unmatched,
+          expert health care
+        </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px]
+        lg:mt-[55px]">
+          < Services />
+
+        </div>
+      </div>
+    </section>
     </>
 
     </>
